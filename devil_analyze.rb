045660eb8f8313f5345c8b2bbfe6495b57b8ac17
Shugo Maeda <shugo@ruby-lang.org>
2016-01-05 15:47:06 +0900

    Refactored.

--- devil_analyze.rb
+++ devil_analyze.rb
@@ -6,9 +6,7 @@ include Statsample
 devils = JSON.parse(ARGF.read)
 obj_name = "防御"
 exp_names = ["体", "速"]
-h = exp_names.each_with_object({
-  obj_name => devils.map {|i| i[obj_name]}
-}) { |name, h|
+h = ([obj_name] + exp_names).each_with_object({}) { |name, h|
   h[name] = devils.map {|i| i[name]}
 }
 puts Statsample::Regression.multiple(h.to_dataset, obj_name).summary
